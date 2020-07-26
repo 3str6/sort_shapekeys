bf15fbeb94475cb87496d5487587f85e01de7232
DESKTOP-9HV8UC6\Estragon <3a5n10ng@gmail.com>
2020-07-26 19:53:07 +0900

    update: add sort type

--- addons/sort_shapekeys.py
+++ addons/sort_shapekeys.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Sort Shapekeys",
     "author": "gogo",
-    "version": (0, 0, 1),
+    "version": (1, 0, 0),
     "blender": (2, 83, 0),
     "description": "Sort shapekeys",
     "warning": "",
@@ -12,14 +12,12 @@ bl_info = {
 
 
 import bpy
-from bpy.types import (
-    Panel,
-)
 from bpy.props import (
     PointerProperty,
+    EnumProperty,
 )
 
-sort_list = [
+sort_FaceCap = [
     "Basis",    # 一番上は必ずBasisにすること
     "browInnerUp",
     "browDown_L",
@@ -75,8 +73,65 @@ sort_list = [
     "tongueOut",
 ]
 
+sort_iFacialMocap = [
+    "Basis",    # 一番上は必ずBasisにすること
+    "browInnerUp",
+    "browDownLeft",
+    "browDownRight",
+    "browOuterUpLeft",
+    "browOuterUpRight",
+    "eyeLookUpLeft",
+    "eyeLookUpRight",
+    "eyeLookDownLeft",
+    "eyeLookDownRight",
+    "eyeLookInLeft",
+    "eyeLookInRight",
+    "eyeLookOutLeft",
+    "eyeLookOutRight",
+    "eyeBlinkLeft",
+    "eyeBlinkRight",
+    "eyeSquintLeft",
+    "eyeSquintRight",
+    "eyeWideLeft",
+    "eyeWideRight",
+    "cheekPuff",
+    "cheekSquintLeft",
+    "cheekSquintRight",
+    "noseSneerLeft",
+    "noseSneerRight",
+    "jawOpen",
+    "jawForward",
+    "jawLeft",
+    "jawRight",
+    "mouthFunnel",
+    "mouthPucker",
+    "mouthLeft",
+    "mouthRight",
+    "mouthRollUpper",
+    "mouthRollLower",
+    "mouthShrugUpper",
+    "mouthShrugLower",
+    "mouthClose",
+    "mouthSmileLeft",
+    "mouthSmileRight",
+    "mouthFrownLeft",
+    "mouthFrownRight",
+    "mouthDimpleLeft",
+    "mouthDimpleRight",
+    "mouthUpperUpLeft",
+    "mouthUpperUpRight",
+    "mouthLowerDownLeft",
+    "mouthLowerDownRight",
+    "mouthPressLeft",
+    "mouthPressRight",
+    "mouthStretchLeft",
+    "mouthStretchRight",
+    "tongueOut",
+]
 
-def check_list(target_object):
+
+
+def check_list(target_object, sort_list):
     missing_list = []
     for shapekey_name in sort_list:
         target_index = target_object.data.shape_keys.key_blocks.find(shapekey_name)
@@ -110,8 +165,14 @@ class SRTSPK_OT_check(bpy.types.Operator):
         return context.object and context.object.data.shape_keys
 
     def execute(self, context):
+        props = context.scene.srtspk
         target_object = context.object
-        missing_list = check_list(target_object)
+        if props.sort_type == "FaceCap":
+            sort_list = sort_FaceCap
+        else:
+            sort_list = sort_iFacialMocap
+
+        missing_list = check_list(target_object, sort_list)
         self.report({'WARNING'}, "Missing: " + str(missing_list))
 
         return {'FINISHED'}
@@ -128,8 +189,14 @@ class SRTSPK_OT_create_missings(bpy.types.Operator):
         return context.object and context.object.data.shape_keys
 
     def execute(self, context):
+        props = context.scene.srtspk
         target_object = context.object
-        missing_list = check_list(target_object)
+        if props.sort_type == "FaceCap":
+            sort_list = sort_FaceCap
+        else:
+            sort_list = sort_iFacialMocap
+
+        missing_list = check_list(target_object, sort_list)
         
         for shapekey_name in missing_list:
             target_object.shape_key_add(name=shapekey_name, from_mix=False)
@@ -148,9 +215,14 @@ class SRTSPK_OT_sort(bpy.types.Operator):
         return context.object and context.object.data.shape_keys
 
     def execute(self, context):
-        # props = context.scene.srtspk
+        props = context.scene.srtspk
         target_object = context.object
-        missing_list = check_list(target_object)
+        if props.sort_type == "FaceCap":
+            sort_list = sort_FaceCap
+        else:
+            sort_list = sort_iFacialMocap
+        
+        missing_list = check_list(target_object, sort_list)
         if missing_list:
             self.report({'WARNING'}, "Missing: " + str(missing_list))
             return {'CANCELLED'}
@@ -163,7 +235,7 @@ class SRTSPK_OT_sort(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class SRTSPK_PT_main(Panel):
+class SRTSPK_PT_main(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "For VRoid"
@@ -172,10 +244,24 @@ class SRTSPK_PT_main(Panel):
     bl_context = 'objectmode'
     
     def draw(self, context):
+        props = context.scene.srtspk
         layout = self.layout
+        layout.prop(props, property="sort_type", text="Type")
         layout.operator(SRTSPK_OT_check.bl_idname, text="Check")
         layout.operator(SRTSPK_OT_create_missings.bl_idname, text="Create Missings")
         layout.operator(SRTSPK_OT_sort.bl_idname, text="Sort")
+
+
+class SRTSPK_props(bpy.types.PropertyGroup):
+    sort_type_input = (
+        ("FaceCap", "FaceCap", ""),      # (識別子, UI表示名, 説明文)
+        ("iFacialMocap", "iFacialMocap", ""),
+    )
+    sort_type: EnumProperty(
+        name="Sort Type",
+        description="Sort Type",
+        items=sort_type_input,
+    )
 
 
 classes = (
@@ -183,6 +269,7 @@ classes = (
     SRTSPK_OT_check,
     SRTSPK_OT_create_missings,
     SRTSPK_PT_main,
+    SRTSPK_props,
 )
 
 
@@ -190,8 +277,11 @@ def register():
     for i in classes:
         bpy.utils.register_class(i)
 
+    bpy.types.Scene.srtspk = PointerProperty(type=SRTSPK_props)
+
 
 def unregister():
+    del bpy.types.Scene.srtspk
     for i in classes:
         bpy.utils.unregister_class(i)
 
